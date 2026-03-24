d978f2ab9565d1f5d9fb9a95f422b51b91a01d93
RJ <rjpatane2210@gmail.com>
2026-03-24 12:10:14 +0530

    Updated allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-key')
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '.onrender.com'
+]
 
 # APPLICATIONS
 INSTALLED_APPS = [
