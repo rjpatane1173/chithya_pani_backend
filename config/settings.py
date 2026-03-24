0bef569d7922d4fd875029fcaf3750455b830132
Rushikesh J Patane <98075435+rjpatane1173@users.noreply.github.com>
2026-03-24 12:50:59 +0530

    Fix CORS

--- config/settings.py
+++ config/settings.py
@@ -125,12 +125,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # CORS
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
 
-# CORS_ALLOWED_ORIGINS = [
-#     "https://chithyapani.vercel.app/"
-# ]
-
+CORS_ALLOWED_ORIGINS = [
+    "https://chithyapani.vercel.app",
+]
+CSRF_TRUSTED_ORIGINS = [
+    "https://chithyapani.vercel.app",
+]
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOW_HEADERS = [
